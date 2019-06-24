e1b81466798c2e696c961390e872102dd18a823a
ribeirolarissa <50153482+ribeirolarissa@users.noreply.github.com>
2019-06-24 20:13:44 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,51 @@
+var questions = [{
+  question: "Qual destas não apresenta uma das finalidades da rastreabilidade?",
+  choices: ['Sigilo de dados', 'Compreender origem dos requisitos', 'Gerenciar escopo do projeto','Avaliar impacto de mudança em um requisito'],
+  correctAnswer: 0,
+  answerLocation: '"https://www.cin.ufpe.br/~gta/rup-vc/core.base_rup/guidances/concepts/traceability_8F6B898D.html"'
+}, {
+  question: "Qual o objetivo principal da Embrapa?",
+  choices: ['Promover inovações, sem se atentar as necessidades do meio rural','Gerar conhecimento e tecnologia para agropecuária brasileira','Desenvolver práticas não sustentáveis','Manter as práticas atuais relacionadas a agropecuária'],
+  correctAnswer: 1,
+  answerLocation: '"https://www.embrapa.br/quem-somos"'
+}, {
+  question: "Qual alternativa encontra-se incorreta a respeito da linguagem Kotlin?",
+  choices: ['É multiplataforma','É desenvolvida pela JetBrains','Tem o nome baseado em uma ilha russa','É interoperável com python'],
+  correctAnswer: 3,
+  answerLocation: '"https://cafeinacodificada.com.br/kotlin/"'
+}, {
+  question: "Qual das opções abaixo não apresenta um programa de visualização de dados?",
+  choices: ['Canva', 'Plot.ly', 'RAW', 'Todos são programas de visualização'],
+  correctAnswer: 3,
+  answerLocation: '"http://observatoriodaimprensa.com.br/jornalismo-na-internet/projeto-avalia-26-softwares-de-visualizacao-de-dados/"'
+}, {
+  question: "Em qual segmento a Duit opera?",
+  choices: ['E-commerce', 'Saúde', 'Segurança', 'Educação'],
+  correctAnswer: 0,
+  answerLocation: '"https://duit.pro/#contact-us"'
+}, {
+  question: "Em qual segmento a Epitrack atua?",
+  choices: ['Energia', 'Leilões', 'Saúde', 'Agronegócios'],
+  correctAnswer: 2,
+  answerLocation: '"https://www.epitrack.tech/about"'
+}, {
+  question: "A linguaguem Kotlin é interoperável com qual outra linguagem?",
+  choices: ['Python','Java','Ruby','C++'],
+  correctAnswer: 1,
+  answerLocation: '"https://www.androidpro.com.br/blog/kotlin/kotlin/#Interoperavel"'
+}, {
+  question: "Qual dos serviços abaixo não é oferecido pela Duit?",
+  choices: ['Consultoria','Segurança da informação','Desenvolvimento','Marketing'],
+  correctAnswer: 1,
+  answerLocation: '"https://duit.pro/#services"'
+}, {
+  question: "Qual a linguagem de programação mais utilizada hoje em dia?",
+  choices: ['Python','JavaScript','Ruby','C'],
+  correctAnswer: 1,
+  answerLocation: '"https://blog.cedrotech.com/10-linguagens-de-programacao-mais-utilizadas-no-mercado-de-tecnologia/"'
+}, {
+  question: "Qual das alternativas apresenta um algoritmo que utiliza chave assimétrica no processo de criptografia?",
+  choices: ['DES','IDEA','RSA','RC'],
+  correctAnswer: 2,
+  answerLocation: '"https://pcworld.com.br/idgnoticia2007-10-040383475254/"'
+}];
